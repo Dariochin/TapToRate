bd2eb135384dd124ee11e2b9a9b1c8613043f8bd
Dariochin <dariochin28@gmail.com>
2024-12-05 11:48:31 +0100

    Initial Commit

---
+++ TapToRate/TapToRateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapToRateApp.swift
+//  TapToRate
+//
+//  Created by d3 on 05/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TapToRateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
